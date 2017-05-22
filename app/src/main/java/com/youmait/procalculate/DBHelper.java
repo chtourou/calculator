f578bda6b2e2d86537d0b78a9c3237c4babcf79e
Tou7a <Tou7a>
2017-05-22 14:56:52 +0100

    firs tcommit

---
+++ app/src/main/java/com/youmait/procalculate/DBHelper.java
@@ -0,0 +1,39 @@
+/*package com.youmait.procalculate;
+
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+import static com.youmait.procalculate.R.id.result;
+
+class DBHelper {
+    private Object readableDatabase;
+
+    public DBHelper(ResultActivity resultActivity) {
+
+        public ArrayList<String> showHistory (String result)
+        {
+            Object db = getReadableDatabase();
+
+            ArrayList<String> list = new ArrayList<String>();
+            int[] selectionArgs = {result};
+
+
+            list.add(resultActivity);
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+    public Object getReadableDatabase() {
+        return readableDatabase;
+    }
+}
+*/
--- app/src/main/java/com/youmait/procalculate/ResultActivity.java
+++ app/src/main/java/com/youmait/procalculate/ResultActivity.java
@@ -2,12 +2,16 @@ package com.youmait.procalculate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ResultActivity extends AppCompatActivity {
 
-    private String[] array  = {"1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3", "104/4 = 26"};
+  private String[] array  = {"1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3","1+2 = 3", "104/4 = 26"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,4 +22,42 @@ public class ResultActivity extends AppCompatActivity {
             (this, R.layout.layout_item, array);
         list.setAdapter(adapter);
     }
+
+
+  /*  private ListView lv;
+    private DBHelper dbHelper;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
+    private String calcName="";
+    private String []EmptyList={"There is  no history yet"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        lv=(ListView)findViewById(R.id.listView);
+        dbHelper=new DBHelper(this);
+        calcName=getIntent().getStringExtra("calcName");
+        list=dbHelper.showHistory(calcName);
+        if(!list.isEmpty())
+            adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        else
+            adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,EmptyList);
+        lv.setAdapter(adapter);
+    }
+
+    public void onClick(View v)
+    {
+
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,EmptyList);
+        lv.setAdapter(adapter);
+    }
+
+*/
+
+
+
 }
